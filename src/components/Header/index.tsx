8acd5cbe59725d423d5ba9710b8adb6f2d3fd9eb
hiigorsilva <higorscontato@gmail.com>
2024-08-03 03:55:24 -0300

    feat: create header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="w-full min-h-32 flex justify-center items-center px-5 bg-zinc-950">
+      <h1 className="font-semibold text-4xl text-zinc-50 -mt-6">
+        Lista de Tarefas
+      </h1>
+    </header>
+  )
+}
