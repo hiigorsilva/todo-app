f19c0b4311e44dace0ebc6230ec7097716e2d328
hiigorsilva <higorscontato@gmail.com>
2024-08-03 03:58:13 -0300

    feat: create footer and home page structure

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className="relative flex justify-center items-center px-5 py-4 bg-zinc-900">
+      <div className="absolute top-0 left-0 right-0 max-w-screen-sm w-full h-px mx-auto border-t border-zinc-800" />
+      <span className="text-sm text-zinc-400">
+        Todos os direitos reservados
+      </span>
+    </footer>
+  )
+}
