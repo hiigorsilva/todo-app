7c7f57ae6bded5b3edf09dc4f6afa296222caafc
hiigorsilva <higorscontato@gmail.com>
2024-08-03 18:29:40 -0300

    feat: create count for each task in the list

--- src/components/TaskCount/index.tsx
+++ src/components/TaskCount/index.tsx
@@ -1,12 +1,13 @@
 type TaskCountProps = {
   text: string
-} 
+  count: number
+}
 
-export const TaskCount = ({ text }: TaskCountProps) => {
+export const TaskCount = ({ text, count }: TaskCountProps) => {
   return (
     <div className="flex items-center gap-1 text-sm text-zinc-200">
       {text}
-      <span className="block w-fit px-1.5 py-0 rounded-lg bg-zinc-800">5</span>
+      <span className="block w-fit px-1.5 py-0 rounded-lg bg-zinc-800">{count}</span>
     </div>
   )
 }
